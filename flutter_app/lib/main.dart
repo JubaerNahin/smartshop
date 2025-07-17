7c38faebf8c0e2805844e0719bc048497bfee984
Jubaer Islam Nahin <jubaernahin17@gmail.com>
2025-07-18 00:35:09 +0600

    init

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,6 +2,11 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/MyApp.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(DevicePreview(enabled: true, builder: (context) => MyApp()));
 }
